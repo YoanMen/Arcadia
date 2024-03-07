de7c881bed9a9d3370ca31ebe7c231f29d61be29
YoanMen <yoan.menard.dev@gmail.com>
2024-03-07 17:45:18 +0100

    continue to implement modal

--- public/assets/scripts/dialogTestimonial.js
+++ public/assets/scripts/dialogTestimonial.js
@@ -3,9 +3,16 @@ const form = document.querySelector(".dialog__form");
 const closeButton = document.querySelector(".dialog__close");
 const addBtn = document.getElementById("add-testimonial");
 const sendBtn = document.getElementById("send-button");
+
+const pseudoInput = document.getElementById("pseudo");
+const messageInput = document.getElementById("message");
+
 const csrf_token = document.head.querySelector(
   'meta[name="csrf-token"]'
 ).content;
+
+pseudoInput.addEventListener("input", CanSend);
+messageInput.addEventListener("input", CanSend);
 
 addBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -29,10 +36,16 @@ dialog.addEventListener("submit", (e) => {
   sendAdvice();
 });
 
-async function sendAdvice() {
-  const pseudoInput = document.getElementById("pseudo").value;
-  const messageInput = document.getElementById("message").value;
+function CanSend() {
+  console.log(messageInput.value.length);
+  if (messageInput.value.length != 0 && pseudoInput.value.length != 0) {
+    sendBtn.disabled = false;
+  } else {
+    sendBtn.disabled = true;
+  }
+}
 
+async function sendAdvice() {
   const r = await fetch("/public/api/advice/send", {
     method: "POST",
     headers: {
@@ -41,14 +54,18 @@ async function sendAdvice() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      pseudo: pseudoInput,
-      message: messageInput,
+      pseudo: pseudoInput.value,
+      message: messageInput.value,
     }),
   });
 
   if (r.ok) {
-    return r.json();
+    form.innerHTML = `<p>Votre avis à été envoyé, il sera visible après validation</p>`;
     // advice send
   }
+
+  console.log(r);
+  form.innerHTML = `<p>Erreur :</p>`;
+
   throw new Error("Cant get advice");
 }
