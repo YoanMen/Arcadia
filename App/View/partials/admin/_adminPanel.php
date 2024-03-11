2e524382f1c11317e9a45a8b4631f86224649ac7
YoanMen <yoan.menard.dev@gmail.com>
2024-03-11 18:36:25 +0100

    begin implementation dashboard

---
+++ App/View/partials/admin/_adminPanel.php
@@ -0,0 +1,19 @@
+<aside class="dashboard__panel">
+  <a class="dashboard__logo" href="">
+    <img height="84px" src="<?= ROOT ?>/assets/images/icons/arcadia-logo.svg" alt="" srcset="">
+  </a>
+  <ul class="dashboard__list">
+    <li class="list__item">
+      <a href=""> <img height="32px" src="<?= ROOT ?>/assets/images/icons/viewDashboard.svg" alt="" srcset="">
+        <span for="">dashboard</span></a>
+    </li>
+    <li class="list__item">
+      <a href=""> <img height="32px" src="<?= ROOT ?>/assets/images/icons/home.svg" alt="" srcset="">
+        <span for="">habitats</span> </a>
+    </li>
+    <li class="list__item">
+      <a href=""> <img height="32px" src="<?= ROOT ?>/assets/images/icons/paw.svg" alt="" srcset="">
+        <span for="">animaux</span> </a>
+    </li>
+  </ul>
+</aside>
---
+++ App/View/partials/admin/_adminTop.php
@@ -0,0 +1,13 @@
+<header class="dashboard__top">
+  <ul>
+    <li>
+      <p>email@email.com</p>
+    </li>
+    <li>
+      <a href="<?= ROOT ?>/logout">
+        <img height="32px" src="<?= ROOT ?>/assets/images/icons/logout.svg" alt="" srcset="">
+
+      </a>
+    </li>
+  </ul>
+</header>
