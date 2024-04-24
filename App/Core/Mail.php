73a1da307676f2a579001e239ef4f08c0fdf3971
YoanMen <yoan.menard.dev@gmail.com>
2024-04-24 20:42:19 +0200

    correcting mail

--- App/Core/Mail.php
+++ App/Core/Mail.php
@@ -8,29 +8,28 @@ class Mail
   public function sendMailToNewUser($to)
   {
     $subject = 'Nouveau compte sur Arcadia';
-    $headers = "Content-type: text/html; charset=UTF-8\r\n";
+    $headers = "MIME-Version: 1.0,\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
     $headers .= "From: yoanmen@alwaysdata.net \r\n";
+    $headers .= "X-Mailer: PHP mail() \r\n";
 
     $message = <<<HTML
-                <!DOCTYPE html>
-                <html lang="fr">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Nouveau compte sur Arcadia</title>
-                </head>
-                <body style="font-family: Arial, Helvetica, sans-serif;
-                      display: flex;
-                      flex-direction: column;
-                      align-items: center;
-                      max-width: 800px;
-                      width: 100%;
-                      margin: 0 auto;">
-                    <h1>Le compte <strong> test@test.com </strong> à été crée</h1>
-                    <p>Afin de pouvoir utiliser votre compte, vous devez vous approcher de l'administrateur afin d'avoir votre mot de passe.</p>
-                    <img style="margin: 0 auto;" width="128px" src="https://yoanmen.alwaysdata.net/assets/images/icons/arcadia-logo.svg" alt="icon zoo">
-                </body>
-                </html>
-                HTML;
+              <!DOCTYPE html>
+              <html lang="fr">
+              <head>
+                  <meta charset="UTF-8">
+                  <title>Nouveau compte sur Arcadia</title>
+              </head>
+              <body style="font-family: Arial, Helvetica, sans-serif;">
+                  <h1>Le compte <strong> test@test.com </strong> à été crée</h1>
+                  <table>
+                      <tr>
+                         <p>Afin de pouvoir utiliser votre compte, vous devez vous approcher de l'administrateur afin d'avoir votre mot de passe.</p>
+                      </tr>
+                  </table>
+              </body>
+              </html>
+              HTML;
 
     $message = str_replace('test@test.com', $to, $message);
 
