06fbf66c4a94e93f039c388957ab16e37e1c3cd0
YoanMen <yoan.menard.dev@gmail.com>
2024-04-24 11:01:00 +0200

    correcting html for mail

--- App/Core/Mail.php
+++ App/Core/Mail.php
@@ -11,33 +11,28 @@ class Mail
     $headers = "Content-type: text/html; charset=UTF-8\r\n";
     $headers .= "From: yoanmen@alwaysdata.net \r\n";
 
-    $message = '<html>
-                  <head>
-                    <title>Calendrier des anniversaires pour Août</title>
-                  </head>
-                  <body style="font-family: Arial, Helvetica, sans-serif;
+    $message = <<<HTML
+                <!DOCTYPE html>
+                <html lang="fr">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Nouveau compte sur Arcadia</title>
+                </head>
+                <body style="font-family: Arial, Helvetica, sans-serif;
                       display: flex;
                       flex-direction: column;
                       align-items: center;
                       max-width: 800px;
                       width: 100%;
                       margin: 0 auto;">
-                    <table>
-                      <tr>
-                        <h1>Le compte test@test.com à été crée</h1>
-                      </tr>
-                      <tr>
-                        <p>Afin de pouvoir utiliser votre compte, vous devez vous approcher de l\'administrateur afin d\'avoir votre mot de passe.</p>
-                      </tr>
-                      <tr>
-                       <img style="margin: 0 auto;" width="320px" class="mobile-menu__logo" src="https://yoanmen.alwaysdata.net/assets/images/icons/arcadia-logo.svg" alt="">
-                      </tr>
-                    </table>
-                  </body>
-                </html>';
+                    <h1>Le compte <strong> test@test.com </strong> à été crée</h1>
+                    <p>Afin de pouvoir utiliser votre compte, vous devez vous approcher de l'administrateur afin d'avoir votre mot de passe.</p>
+                    <img style="margin: 0 auto;" width="128px" src="https://yoanmen.alwaysdata.net/assets/images/icons/arcadia-logo.svg" alt="icon zoo">
+                </body>
+                </html>
+                HTML;
 
     $message = str_replace('test@test.com', $to, $message);
-
 
     if (!mail($to, $subject, $message, $headers)) {
       $_SESSION['error'] =  "Impossible d'envoyer l'email au nouvelle utilisateur";
